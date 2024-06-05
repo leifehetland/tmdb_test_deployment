0ab46e977e8e18e67e7a2c27676cd6e147e4ae17
leifehetland <lehetland1@gmail.com>
2024-06-05 12:47:41 -0400

    testing deployment

---
+++ src/utils/API.js
@@ -0,0 +1,17 @@
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTNhYzgxMjM0NDMwZjE0YjQ4MjQ1OWIxNzA4N2Y1ZSIsInN1YiI6IjY0MmM3MWVlYzBhMzA4MDA3NzRmMzk5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O_lMcdb3vmdE6CI79jxUcX81avN6J39Y3IxxTPYzAbU'
+  }
+};
+
+
+const search = (query) =>
+  fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, options)
+  .then(response => response.json())
+  .then(response => response)
+  .catch(err => console.error(err));
+
+// Export an object with a "search" method that searches the Giphy API for the passed query
+export default search;
