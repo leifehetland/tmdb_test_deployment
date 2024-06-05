bfb530c4d36e4bd1c91a7893cab09a64ca4234a1
leifehetland <lehetland1@gmail.com>
2024-06-05 14:37:08 -0400

    testing with OMDBAPI

--- src/components/SearchResultContainer.jsx
+++ src/components/SearchResultContainer.jsx
@@ -18,7 +18,7 @@ const SearchResultContainer = () => {
   // We want to run this method when the component first loads so that we have images of kittens to display
   // The second argument is the dependency array. This means that this method will only run when the component first loads
   useEffect(() => {
-    searchGiphy('Furiosa');
+    searchGiphy('The Seventh Seal');
   }, []);
 
   return (
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,14 +1,19 @@
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTNhYzgxMjM0NDMwZjE0YjQ4MjQ1OWIxNzA4N2Y1ZSIsInN1YiI6IjY0MmM3MWVlYzBhMzA4MDA3NzRmMzk5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O_lMcdb3vmdE6CI79jxUcX81avN6J39Y3IxxTPYzAbU'
-  }
-};
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTNhYzgxMjM0NDMwZjE0YjQ4MjQ1OWIxNzA4N2Y1ZSIsInN1YiI6IjY0MmM3MWVlYzBhMzA4MDA3NzRmMzk5OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O_lMcdb3vmdE6CI79jxUcX81avN6J39Y3IxxTPYzAbU'
+//   }
+// };
 
+// const search = (query) =>
+//   fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, options)
+//   .then(response => response.json())
+//   .then(response => response)
+//   .catch(err => console.error(err));
 
 const search = (query) =>
-  fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`, options)
+  fetch(`http://www.omdbapi.com/?t=${query}&apikey=4621997e`)
   .then(response => response.json())
   .then(response => response)
   .catch(err => console.error(err));
